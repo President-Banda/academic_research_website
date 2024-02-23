cc5728c77b0dba6db9c13a7ab1581621860703fd
president <abanda435@gmail.com>
2024-02-23 07:17:06 +0200

    added initial header and footer, content alignment

--- app/page.js
+++ app/page.js
@@ -1,5 +1,20 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
 export default function Home() {
   return (
-    <h1>Academic Research Website</h1>
+      <>
+        <Header />
+        <div class="grid grid-cols-1 gap-4 lg:grid-cols-[120px_1fr] lg:gap-8 mx-52 my-14">
+          <div class="h-32 rounded-lg bg-gray-200">
+            Name and Photo Photo
+          </div>
+          <div class="h-32 rounded-lg bg-gray-200">
+            Content
+          </div>
+        </div>
+
+
+        <Footer />
+      </>
       );
 }
